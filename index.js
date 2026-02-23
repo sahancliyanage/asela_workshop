f628dbb20c8138e55ab4972b14895718b48bd0d2
sahancliyanage <chamikaliyanage2002322@gmail.com>
2026-02-23 10:19:49 +0530

    Remove node_modules from tracking + update .gitignore

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
+
+app.post('/submit', (req, res) => {
+    res.send('Form submitted successfully!');
+});
